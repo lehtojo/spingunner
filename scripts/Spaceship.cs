176f1a044d0b16a8dce573cae1fd452ffbc9c39e
Joonas Lehto <joonas.eemil.lehto@gmail.com>
2024-06-04 19:10:57 +0300

    Add camera shake

--- scripts/Spaceship.cs
+++ scripts/Spaceship.cs
@@ -14,6 +14,15 @@ public partial class Spaceship : RigidBody2D
     public float RecoilForce { get; set; }
 
     [Export]
+    public float MaxSpeed { get; set; }
+
+    [Export]
+    public float ShakeStrength { get; set; } = 32.0f;
+
+    [Export]
+    public float ShakeFade { get; set; } = 5.0f;
+
+    [Export]
     public float ShootCooldown { get; set; }
 
     [Export]
@@ -26,6 +35,7 @@ public partial class Spaceship : RigidBody2D
     public float ProjectileSpeed { get; set; }
 
     private float ShootCooldownRemaining { get; set; }
+    private float CurrentShakeStrength { get; set; }
 
     private void Shoot()
     {
@@ -40,13 +50,26 @@ public partial class Spaceship : RigidBody2D
         projectile.Rotation = Rotation;
         Root?.AddChild(projectile);
 
+        // Start shaking the camera
+        CurrentShakeStrength = ShakeStrength;
+
         // Apply opposite recoil force to ourselves
         LinearVelocity += -direction * RecoilForce;
+        LinearVelocity = LinearVelocity.Normalized() * MathF.Min(LinearVelocity.Length(), MaxSpeed);
     }
 
     public override void _Ready()
     {
         LinearVelocity = Utils.RandomDirection() * RecoilForce;
+    }
+
+    public override void _Process(double delta)
+    {
+        if (CurrentShakeStrength > 0)
+        {
+            CurrentShakeStrength = Mathf.Lerp(CurrentShakeStrength, 0, ShakeFade * (float)delta);
+            GetTree().Root.GetNode<Camera2D>("Root/Camera").Offset = Utils.RandomDirection() * CurrentShakeStrength;
+        }
     }
 
     public override void _PhysicsProcess(double delta)
@@ -56,7 +79,7 @@ public partial class Spaceship : RigidBody2D
         // The player can steer the spaceship using the main cannon's recoil velocity
         ShootCooldownRemaining -= (float)delta;
 
-        if (ShootCooldownRemaining <= 0.0f && Input.IsActionJustPressed("Shoot"))
+        if (ShootCooldownRemaining <= 0.0f && Input.IsActionPressed("Shoot"))
         {
             ShootCooldownRemaining = ShootCooldown;
             GD.Print("Shooting");
